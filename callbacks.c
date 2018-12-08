de9c04edcb998b5fa5e536e7962c2a8b0e22fc1a
raya1995 <44503525+raya1995@users.noreply.github.com>
2018-12-08 16:45:44 +0100

    Add files via upload

---
+++ callbacks.c
@@ -0,0 +1,34 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_button1_clicked                     (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_bitton2_clicked                     (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_button1_clicked                     (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ callbacks.h
@@ -0,0 +1,14 @@
+#include <gtk/gtk.h>
+
+
+void
+on_button1_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_bitton2_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button1_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
